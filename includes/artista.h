dc6c290999183ad3c651ee6b2f306a9d574e5ef4
Hermeson Alves <hermesonalves256@gmail.com>
2025-03-22 17:53:30 -0300

    começo artista

--- includes/artista.h
+++ includes/artista.h
@@ -17,5 +17,14 @@ typedef struct ARTISTA{
 
 
 ARTISTA *criar_artista(char *nome, char *tipo, char *estilo_musical);
+ARTISTA *buscar_artista(ARTISTA *raiz, char *nome);
+ARTISTA *inserir_artista(ARTISTA *raiz, ARTISTA *novo);
+ARTISTA *remover_artista(ARTISTA *raiz, char *nome);
+ARTISTA *remover_artista_recursivo(ARTISTA *raiz, char *nome);
+
+ARTISTA *criar_artista(char *nome, char *tipo, char *estilo_musical){
+    ARTISTA *novo = (ARTISTA *) malloc(sizeof(ARTISTA));
+    novo->nome = nome;
+}
 
 #endif
