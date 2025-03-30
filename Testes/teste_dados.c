5b6259e32da21be59019f32400226ba7dfd95f06
MarcosWillianAP1K <mwss@outlook.com>
2025-03-30 18:15:13 -0300

    Depois testar os dados

--- Testes/teste_dados.c
+++ Testes/teste_dados.c
@@ -7,14 +7,7 @@
 
 int main()
 {
-    DADOS *dados = alocar_dados();
-    dados->artista = criar_artista("Artista Teste1", "Artista Teste2", "Artista Teste3", 0, NULL);
-
-    printf("Tamanho da string: %d\n", sizeof (dados->artista->estilo_musical));
-
-    printar_dados(dados);
-    liberar_dados(dados);
-    // Teste de alocação e liberação de dados
+    //DEPOIS TESTAR DADOS AGORA NÃO
 
 
 
