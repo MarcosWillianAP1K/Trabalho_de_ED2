b35faedb918c0f1b349435c6d7c8cf9d8d251a56
MarcosWillianAP1K <mwss@outlook.com>
2025-04-03 19:49:30 -0300

    Adc teste

---
+++ Testes/teste_arv_binaria.c
@@ -0,0 +1,17 @@
+#include "../includes/arv_binaria.h"
+#include "../includes/funcao_sistema.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    ARV_BINARIA *raiz = NULL;
+
+    inicializar_arv_binaria(&raiz);
+
+
+
+
+    return 0;
+}
