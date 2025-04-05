c372933edc5f13b62eff5893b4dd054a533a94d3
MarcosWillianAP1K <mwss@outlook.com>
2025-04-05 16:43:48 -0300

    Compilador feito

---
+++ compilador.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int compilador = system("gcc ../src/main.c ../Tads/album.c ../Tads/artista.c ../Tads/arv_binaria.c ../Tads/funcao_sistema.c ../Tads/interatividade.c ../Tads/func_interatividade.c ../Tads/musica_playlist.c ../Tads/musica.c ../Tads/playlist.c ../Tads/dados.c -o compilador");
+    
+    if (compilador == 0)
+    {
+        system("./compilador");
+    }
+
+    return 0;
+}
