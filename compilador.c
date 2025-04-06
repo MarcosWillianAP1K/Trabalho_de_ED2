5c1ba37a9ce023a0ffba331e0ead066c1ce3686f
MarcosWillianAP1K <mwss@outlook.com>
2025-04-05 21:26:14 -0300

    Resolvido a compilação

--- compilador.c
+++ compilador.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Comando executa pelo CMD o compilador GCC para compilar os arquivos C e gerar um executável
+
 int main()
 {
-    int compilador = system("gcc -o compilador ../src/main.c ../Tads/album.c ../Tads/artista.c ../Tads/arv_binaria.c ../Tads/funcao_sistema.c ../Tads/interatividade.c ../Tads/func_interatividade.c ../Tads/musica_playlist.c ../Tads/musica.c ../Tads/playlist.c ../Tads/dados.c ");
+    int compilador = system("gcc -o executavel ../src/main.c ../Tads/album.c ../Tads/artista.c ../Tads/arv_binaria.c ../Tads/funcao_sistema.c ../Tads/interatividade.c ../Tads/func_interatividade.c ../Tads/musica_playlist.c ../Tads/musica.c ../Tads/playlist.c ../Tads/dados.c ");
     
     if (compilador == 0)
     {
-        // system("./compilador");
+        system("executavel.exe");
     }
 
     return 0;
