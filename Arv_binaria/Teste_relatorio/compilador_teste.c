271bd1fdb44f6f2937f23aa2d2812df92f175da2
MarcosWillianAP1K <mwss@outlook.com>
2025-04-18 17:01:09 -0300

    Compilador de testes

---
+++ Arv_binaria/Teste_relatorio/compilador_teste.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Comando executa pelo CMD o compilador GCC para compilar os arquivos C e gerar um executável
+
+int main()
+{
+    int compilador = 1;
+    
+    compilador = system("gcc -o executavel_teste ../iniciar_testes.c ../../Tads/album.c ../../Tads/artista.c ../../Tads/arv_binaria.c ../../Tads/funcao_sistema.c  ../../Tads/func_interatividade.c ../../Tads/musica_playlist.c ../../Tads/musica.c ../../Tads/playlist.c ../../Tads/dados.c ");
+    
+    if (compilador == 0)
+    {
+        system("executavel_teste");
+    }
+
+    return 0;
+} 
